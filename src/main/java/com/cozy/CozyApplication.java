f4abaa61efce711f79d134213205ce95ad5616d7
didrlgus <rlgusdid@naver.com>
2021-09-16 22:02:22 +0900

    feat: spring boot 초기 세팅 추가

---
+++ src/main/java/com/cozy/CozyApplication.java
@@ -0,0 +1,13 @@
+package com.cozy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CozyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CozyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cozy/CozyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cozy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CozyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
