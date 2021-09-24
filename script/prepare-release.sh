efd688bf1efb3613977e8b8f10f743bf145dcc6a
didrlgus <rlgusdid@naver.com>
2021-09-25 02:17:49 +0900

    feat: 개발 DB, 테스트 DB 분리
    - spring profile로 개발 DB, 테스트 DB 환경 분리
    - datasource 관련 민감한 정보 github action에 세팅

--- script/prepare-release.sh
+++ script/prepare-release.sh
@@ -2,11 +2,20 @@
 
 echo "version = $1"
 
+# Get version number from version tag
 JAR_VERSION=`echo $1 | cut -d'v' -f2`
 echo "jar = $JAR_VERSION"
 
 chmod +x gradlew
-./gradlew -Pversion=$JAR_VERSION build
+./gradlew -Pversion=$JAR_VERSION build \
+          -DEV_MYSQL_URL=$DEV_MYSQL_URL \
+          -DEV_MYSQL_PORT=$DEV_MYSQL_PORT \
+          -MYSQL_DATABASE_NAME=$MYSQL_DATABASE_NAME \
+          -DEV_MYSQL_USERNAME=$DEV_MYSQL_USERNAME \
+          -DEV_MYSQL_PASSWORD=$DEV_MYSQL_PASSWORD \
+          -H2_USERNAME=H2_USERNAME \
+          -H2_PASSWORD=H2_PASSWORD
+
 mkdir release && cp build/libs/*.jar release
 
 printenv
--- src/test/java/com/cozy/CozyApplicationTests.java
+++ src/test/java/com/cozy/CozyApplicationTests.java
@@ -4,18 +4,16 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 /**
- *
  * @SpringBootTest는 테스트에 사용할 ApplicationContext를 생성하고 빈들을 불러오는 역할을 수행한다.
  * @SpringBootTest를 별다른 옵션을 지정하지 않고 이용하면 해당 애플리케이션 상에 정의된 모든 빈을 ApplicationContext로 불러온다.
  * 또한, classes 옵션을 이용해 빈을 생성할 @Configuration 클래스를 직접 지정할 수도 있다.
- *
  */
 
 @SpringBootTest
 class CozyApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+  @Test
+  void contextLoads() {
+  }
 
 }
